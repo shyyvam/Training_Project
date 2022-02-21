fd7adb36ade9bb36d92cc17ccb780f0097f3eee0
SHIVAM SHARMA <51492085+shyyvam@users.noreply.github.com>
2022-02-21 11:33:24 +0530

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+
+    $database = "rockpaper";
+
+     // Create a connection
+     $conn = mysqli_connect($servername,
+         $username, $password, $database);
+
+
+
+    if($conn) {
+        echo "success";
+    }
+    else {
+        die("Error". mysqli_connect_error());
+    }
+  ?>
